e2f49f06bcef724c7cd8403a57e7971ec39a4759
Evelyne74 <eve.fournier@free.fr>
2024-04-17 16:03:28 +0200

    shorter

--- main.R
+++ main.R
@@ -2,11 +2,11 @@ library(tidyverse)
 library(here)
 
 
-tv_hours_table<-gss_cat %>% 
+tv_hours_short<-gss_cat %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours=mean(tvhours,na.rm=TRUE))
 
-write_csv(tv_hours_table, here("TV_Hours_By_Marital_Status.csv"))
+write_csv(tv_hours_short, here("TV_Hours_By_Marital_Status.csv"))
 
 
 
